9f081ce3702f4fc022d165a5e3416b67d37e6b72
V4lued <164889818+V4lued@users.noreply.github.com>
2025-07-24 18:30:04 +0800

    Update jest.setup.cjs

--- jest.setup.cjs
+++ jest.setup.cjs
@@ -1,10 +1,18 @@
 require('@testing-library/jest-dom');
 
 // Polyfill ResizeObserver for ReactFlow and other components
+// eslint-disable-next-line
 if (typeof window !== 'undefined' && !window.ResizeObserver) {
+  // The following methods are intentionally empty for Jest testing environment
+  // eslint-disable-next-line no-empty-function
   window.ResizeObserver = class {
-    observe() {}
-    unobserve() {}
-    disconnect() {}
+    observe() {}      // Intentionally empty for test polyfill
+    unobserve() {}    // Intentionally empty for test polyfill
+    disconnect() {}   // Intentionally empty for test polyfill
   };
-} 
+}
+
+// Clear all timers after each test to prevent leaks
+afterEach(() => {
+  jest.clearAllTimers();
+}); 
